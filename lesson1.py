e0421d8aaa6d38884cb70260929fb6fd32e53d61
sg5555uu <sg5555@mail.ru>
2024-07-06 20:04:22 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+n31 = 1234
+n32 = 5678
+print(n31%1000//10 + n32%1000//10)
+
+# 4th program
+n41 = 13.42
+n42 = 42.13
+print(int(n41//1) == int(n42*100%100) or int(n42//1) == int(n41*100%100))
