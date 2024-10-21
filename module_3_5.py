b7bf11395ed6e01baabe2a27b3f49b1805b9a805
sg5555uu <sg5555@mail.ru>
2024-10-21 06:30:16 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,12 +1,19 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     str_no_zeroes = str_number.replace('0', '')
-    first = int(str_no_zeroes[0])
-    if len(str_no_zeroes) < 2:
-        return first
+    if str_no_zeroes == '':
+        return 0
     else:
-        return first * get_multiplied_digits(int(str_no_zeroes[1:]))
+        first = int(str_no_zeroes[0])
+        if len(str_no_zeroes) < 2:
+            return first
+        else:
+            return first * get_multiplied_digits(int(str_no_zeroes[1:]))
 
 
-result = get_multiplied_digits(402030)
+result = get_multiplied_digits(40203)
+result1 = get_multiplied_digits(420)
+result2 = get_multiplied_digits(0)
 print(result)
+print(result1)
+print(result2)
